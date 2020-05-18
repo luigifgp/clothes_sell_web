e62a2edfee7a8d17375a6f5108ce90db85de3793
ferluigi <luigilazer12@gmail.com>
2020-05-18 12:43:01 +0100

    add reselect new things for the cart-dropdown

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import {connect} from 'react-redux';
-
+ import { selectCartItemsCount, selectCartItems } from "../../redux/cart/cart.selectors";
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
 import {  ReactComponent as ShoppingIcon  } from "../../assets/shopping-bag.svg";
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon"/>
-    <span className="item-count"></span>
+    <span className="item-count">{itemCount}</span>
 
     </div>
 
@@ -18,5 +18,10 @@ const mapDispatchToProps = dispatch =>({
     toggleCartHidden: () => dispatch(toggleCartHidden(mapDispatchToProps))
 });
 
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
